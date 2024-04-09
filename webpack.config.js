61ac6ef0aa13a29158d5c9e5f44cf609d677dbbc
Bartosz <119621854+Izentropowy@users.noreply.github.com>
2024-04-09 19:13:46 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/[name][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.gif$/,
+        type: "asset/inline",
+      },
+      {
+        test: /\.(ttf|eot|svg)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      config$: "./configs/app-config.js",
+      react: "./vendor/react-master",
+    },
+    extensions: [".js", ".jsx"],
+    modules: [
+      "node_modules",
+      "bower_components",
+      "shared",
+      "/shared/vendor/modules",
+    ],
+  },
+};
