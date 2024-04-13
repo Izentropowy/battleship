2b08fdb28ab93921a44e8e96884d84a2741f58a8
Izentropowy <119621854+Izentropowy@users.noreply.github.com>
2024-04-13 12:23:50 +0200

    create ship class

---
+++ src/factories/ship.js
@@ -0,0 +1,20 @@
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.isSunk = false;
+  }
+
+  hit() {
+    this.hits++;
+    this.#checkStatus();
+  }
+
+  #checkStatus() {
+    if (this.hits === this.length) {
+      this.isSunk = true;
+    }
+  }
+}
+
+export default Ship;
